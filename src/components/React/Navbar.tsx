f21493f2fd1a0c069a83c5b136c1f7ac00ba0e77
adi <guddumandal605605@gmail.com>
2026-05-07 18:47:30 +0530

    landing

--- src/components/React/Navbar.tsx
+++ src/components/React/Navbar.tsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react";
+import lang from "../../data/language.json";
+import { IoLanguage } from "react-icons/io5";
 
 const Navbar = () => {
   const NavPath: Array<string> = ["Home", "abouts", "products"];
   const [Current, SetCurrent] = useState<string>("");
+  const [CurrentLang, SetCurrentLang] = useState<string>("");
 
   useEffect(() => {
     const path = window.location.pathname.split("/")[1];
     SetCurrent(path !== "" ? path : "Home");
-  }, []);
+    console.log(CurrentLang);
+  }, [CurrentLang]);
 
   return (
     <>
       <div className="flex md:gap-8 gap-5 justify-between items-center max-md:text-xs p-3 px-5">
-        <div></div>
-        <div className="flex gap-5 text-white p-3 rounded-xl md:px-10 ml-10">
+        <div>
+          <div className="w-10 h-10 rounded-full bg-transparent overflow-hidden drop-shadow-2xl drop-shadow-violet-400/40 hover:drop-shadow-violet-400 transition duration-200 ease-in-out">
+            <img
+              src="/logo/Logo.png"
+              alt="logo"
+              className="relative scale-150 top-1"
+            />
+          </div>
+        </div>
+        <div className="flex gap-5 text-white p-3 rounded-xl md:px-10 lg:ml-10">
           {NavPath.map((i, index) => (
             <a
               key={index}
@@ -24,12 +36,19 @@ const Navbar = () => {
             </a>
           ))}
         </div>
-        <div className="md:w-15 md:h-15 w-10 h-10 rounded-full bg-transparent overflow-hidden drop-shadow-2xl drop-shadow-violet-400/40 hover:drop-shadow-violet-400 transition duration-200 ease-in-out">
-          <img
-            src="/logo/Logo.png"
-            alt="logo"
-            className="relative top-1.5 scale-120"
-          />
+        <div className="flex gap-3 items-center">
+          <IoLanguage className="text-white" />
+          <select
+            className="text-white outline-0"
+            defaultValue={lang[0]}
+            onChange={(e) => SetCurrentLang(e.target.value)}
+          >
+            {lang.map((i, index) => (
+              <option className="text-black" value={i} key={index}>
+                {i}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
     </>
--- src/data/lang/content_en.ts
+++ src/data/lang/content_en.ts
@@ -8,18 +8,18 @@ export const siteContent = {
     },
     cards: [
       {
-        id: "mission",
-        title: "Our Mission",
-        tagline: "Engine Agnostic. Developer Focused.",
-        content:
-          "We bridge the gap between imagination and implementation. While we have a deep love for Godot, our assets are engineered for universal compatibility across Unity, Unreal, and custom frameworks.",
-      },
-      {
         id: "assets",
         title: "Asset Quality",
         tagline: "Battle-Tested Assets",
         content:
           "Every mesh is optimized for high FPS, featuring PBR workflows, clean topology, and plug-and-play Godot scenes.",
+      },
+      {
+        id: "mission",
+        title: "Our Mission",
+        tagline: "Engine Agnostic. Developer Focused.",
+        content:
+          "We bridge the gap between imagination and implementation. While we have a deep love for Godot, our assets are engineered for universal compatibility across Unity, Unreal, and custom frameworks.",
       },
       {
         id: "internal-ip",
@@ -33,6 +33,11 @@ export const siteContent = {
       performance: "Optimized draw calls and LODs included.",
       formats: "Native support for .tscn, .gltf, and .blend.",
       modular: "Snap-to-grid kits for rapid prototyping.",
+    },
+    contact: {
+      yt: "",
+      itech: "",
+      email: "",
     },
   },
 };
